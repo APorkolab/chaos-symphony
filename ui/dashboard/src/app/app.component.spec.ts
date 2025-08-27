f4559e68ad7dedc89a99db3a9a6ba0906ff6295c
Adam Porkolab <73812292+APorkolab@users.noreply.github.com>
2025-08-27 15:17:32 +0200

    Update app.component.spec.ts

--- ui/dashboard/src/app/app.component.spec.ts
+++ ui/dashboard/src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { AppComponent } from './app.component';
+// Opcionális pluszok, ha kell router vagy HTTP a template-ben/injektálásban:
+// import { provideRouter } from '@angular/router';
+// import { provideHttpClient } from '@angular/common/http';
 
-describe('AppComponent', () => {
+describe('AppComponent (standalone)', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [AppComponent],
+      imports: [AppComponent],
+      // providers: [provideRouter([]), provideHttpClient()],
+      schemas: [NO_ERRORS_SCHEMA], // ignorálja az ismeretlen tageket/attribútumokat CI-ben
     }).compileComponents();
   });
 
